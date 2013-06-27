a5c1634d33cb4a47128dedabc1a92f4a1e42c3d4
elnaqah <eng.ahmed.medhat@gmail.com>
2013-06-27 05:16:36 +0200

    Initial Commit

---
+++ NQFoldingView/NQAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  NQAppDelegate.h
+//  NQFoldingView
+//
+//  Created by AhmedElnaqah on 6/27/13.
+//  Copyright (c) 2013 elnaqah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class NQViewController;
+
+@interface NQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) NQViewController *viewController;
+
+@end
---
+++ NQFoldingView/NQViewController.h
@@ -0,0 +1,13 @@
+//
+//  NQViewController.h
+//  NQFoldingView
+//
+//  Created by AhmedElnaqah on 6/27/13.
+//  Copyright (c) 2013 elnaqah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NQViewController : UIViewController
+
+@end
